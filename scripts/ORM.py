7ef27fdf8349d578cd820eb954ab079818b3c6fb
Ruochen Wu <97808991+Rc-W024@users.noreply.github.com>
2023-06-10 17:00:07 +0200

    Add files via upload

---
+++ scripts/ORM.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# create a database engine
+engine=create_engine('postgresql://postgres:XXXXX@localhost:5432/postgres')
+
+# create a session factory
+Session=sessionmaker(bind=engine)
+
+# create base class
+Base=declarative_base()
+
+# define the model class
+class SimData(Base):
+    __tablename__='sim_data'
+
+    id=Column(Integer,primary_key=True)
+    username=Column(String)
+    password=Column(String)
+    geom=Column(String)
+
